6dd007958ece760168100450cbf9d63c3d7a8401
Your Name <iankibet87@gmail.com>
2023-06-26 08:30:19 +0800

    function lab

---
+++ index.js
@@ -0,0 +1,34 @@
+// describe("shout(string)", function () {
+//     it("receives one argument and returns it in all caps", function () {
+//       expect(shout("hello")).toEqual("HELLO");
+//     });
+//   });
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === "Let's have dinner together!") {
+        return "I would love to!";
+  }
+}
+
+// function sayHiToHeadphonedRoommate(string) {
+//     if (string === string.toLowerCase()) {
+//       return "I can't hear you!";
+//     } else {
+//       return "I can hear you!";
+//     }
+//   }
+function sayHiToHeadphonedRoommate(string) {
+    return "I can\'t hear you!"
+}
